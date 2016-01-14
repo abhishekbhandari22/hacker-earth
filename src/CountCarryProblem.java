43fb3ff3f86b0b0edb5f9f29739e99f90e613109
abhishekbhandari22 <abhishek.nsit22@gmail.com>
2016-01-14 20:18:18 +0530

    update solution as per conditions

--- src/CountCarryProblem.java
+++ src/CountCarryProblem.java
@@ -1,6 +1,9 @@
-import java.io.InputStreamReader;
+/*
+ * Works completely fine 
+ * Created By Abhishek Bhandari, 14/01/2016
+  */
 import java.util.Scanner;
-
+import java.io.InputStreamReader;
 
 class CountCarryProblem {
 	public static void main(String[] args){
@@ -9,13 +12,16 @@ class CountCarryProblem {
 		int N = sc.nextInt();
 		while(N-->0){
 			String a = sc.next();
-			while((a.length()<3))
-				a="0"+a;
 			String b = sc.next();
-			while(b.length()<3)
-				b="0"+b;
+			if(a.length()<b.length())
+				while((a.length()<b.length()))
+					a="0"+a;
+			else
+				while(b.length()<a.length())
+					b="0"+b;
+			
 			int count =0;
-			for(int i=0;i<3;i++){
+			for(int i=0;i<a.length();i++){
 				int ai=Integer.parseInt(a.substring(i, i+1));
 				int bi=Integer.parseInt(b.substring(i, i+1));
 				if((ai+bi)>9)
